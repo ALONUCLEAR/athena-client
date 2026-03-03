e7ebc5e1b30867ea687571d3b16e0c0658213618
liorLahav <lior.lahav10@gmail.com>
2026-03-03 19:16:33 +0200

    feat: enhance generic search component with options and favorites functionality

--- src/app/generic-search/generic-search-option/generic-search-option.component.ts
+++ src/app/generic-search/generic-search-option/generic-search-option.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { searchOption } from '../generic-search.component';
 
 @Component({
   selector: 'app-generic-search-option',
   templateUrl: './generic-search-option.component.html',
-  styleUrls: ['./generic-search-option.component.less']
+  styleUrls: ['./generic-search-option.component.less'],
 })
-export class GenericSearchOptionComponent {}
+export class GenericSearchOptionComponent {
+  @Input() option!: searchOption;
+  @Input() isFavoriteSearch: boolean = false;
+  @Input() isMultiChoise: boolean = false;
+  @Input() isFavorite: boolean = false;
+
+  @Output() onFavoriteChange: EventEmitter<string> = new EventEmitter<string>();
+  onStarClick() {
+    this.onFavoriteChange.emit(this.option.title);
+  }
+}
--- src/app/generic-search/generic-search.component.ts
+++ src/app/generic-search/generic-search.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface searchOption {
+  title: string;
+  isFavorite?: boolean;
+}
 
 @Component({
   selector: 'app-generic-search',
   templateUrl: './generic-search.component.html',
-  styleUrls: ['./generic-search.component.less']
+  styleUrls: ['./generic-search.component.less'],
 })
 export class GenericSearchComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() options: searchOption[] = [];
+  @Input() isFavoriteSearch: boolean = false;
+  @Input() isMultiChoise: boolean = false;
 
+  searchValue = '';
+  matchingOptions: searchOption[] = [];
   ngOnInit(): void {
+    this.matchingOptions = this.options;
+    if (this.isFavoriteSearch) {
+      this.favoriteSort();
+    }
   }
 
+  favoriteSort() {
+    setTimeout(() => {
+      const favorites: searchOption[] = this.matchingOptions
+        .filter((option) => option?.isFavorite)
+        .sort();
+      const noFavorites: searchOption[] = this.matchingOptions
+        .filter((option) => !option?.isFavorite)
+        .sort();
+      this.matchingOptions = [...favorites, ...noFavorites];
+      // delay because the list updates too fast
+    }, 200);
+  }
+
+  onSearchValueChange(input: string) {
+    this.matchingOptions = this.options.filter((option) =>
+      option.title.startsWith(input),
+    );
+    if (this.isFavoriteSearch) {
+      this.favoriteSort();
+    }
+  }
+
+  onFavoriteOptionChange(title: string) {
+    const option = this.options.find((o) => o.title === title);
+    if (option) option.isFavorite = !option.isFavorite;
+    this.favoriteSort();
+    // TODO : add a service that update the s3 I guess with this choise
+  }
 }
